7c71113224fe105e54918fa0e0db5056071cc1df
halimasayyed <halima.sayyed@atriina.com>
2021-01-01 22:07:04 +0530

    set checked person name and date

--- ethal/ethal/doctype/payment_request_and_authorization/payment_request_and_authorization.js
+++ ethal/ethal/doctype/payment_request_and_authorization/payment_request_and_authorization.js
@@ -16,5 +16,18 @@ frappe.ui.form.on('Payment Request and Authorization', {
 		  .success(success =>{
 		  })
 	   }
+   },
+   on_load: function(frm){
+	if(frm.doc.workflow_state == 'Sent For Approval'){
+		frappe.call({
+			method: "ethal.ethal.doctype.payment_request_and_authorization.payment_request_and_authorization.set_approver_name",
+			args: {
+				data: frm.doc
+			}
+		})
+		.success(success =>{
+		})
+
+	} 
    }
 });
--- ethal/ethal/doctype/payment_request_and_authorization/payment_request_and_authorization.py
+++ ethal/ethal/doctype/payment_request_and_authorization/payment_request_and_authorization.py
@@ -3,8 +3,21 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+import json
 
 class PaymentRequestandAuthorization(Document):
 	pass
+
+@frappe.whitelist()
+def set_approver_name(data):
+    data=json.loads(data)
+    
+    get_approver_name = frappe.db.get_value('Comment', {'reference_name':data['name'], 'content': 'Send for Approval'}, 'owner')
+    
+    get_approved_date = frappe.db.get_value('Comment', {'reference_name':data['name'], 'content': 'Send for Approval'}, 'modified')
+    
+    frappe.db.set_value(data['doctype'], {'name': data['name']}, 'checked_person', get_approver_name)
+    frappe.db.set_value(data['doctype'], {'name': data['name']}, 'checked_date', get_approved_date)
+    frappe.db.commit()
