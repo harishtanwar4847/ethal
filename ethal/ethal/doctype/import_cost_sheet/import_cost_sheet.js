9d100638da6028ad3dc1a59f9ff3b8754ff99119
halimasayyed <halima.sayyed@atriina.com>
2021-01-21 00:46:52 +0530

    create items child table

--- ethal/ethal/doctype/import_cost_sheet/import_cost_sheet.js
+++ ethal/ethal/doctype/import_cost_sheet/import_cost_sheet.js
@@ -2,19 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Import Cost Sheet', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
-	// setup: function(frm){
-	// 	var l = ['Invoice Value', 'Sea Fright (ETB)', 'Inland Fright (ETB)', 'Insurance (ETB)', 'Import Customs Duty (ETB)', 'Other (ETB)', 'Bank charge (ETB)', 'Storage (ETB)', 'Port handling charge (ETB)', 'Transit and clearing (ETB)', 'Loading & unloading (ETB)', 'Inland transport (ETB)', 'Miscellaneous (ETB)']
+	},
+	setup: function(frm){
+		if (frm.doc.import_cost_sheet_items.length != 12) {
+		var l = ['Sea Fright (ETB)', 'Inland Fright (ETB)', 'Insurance (ETB)', 'Import Customs Duty (ETB)', 'Other (ETB)', 'Bank charge (ETB)', 'Storage (ETB)', 'Port handling charge (ETB)', 'Transit and clearing (ETB)', 'Loading & unloading (ETB)', 'Inland transport (ETB)', 'Miscellaneous (ETB)']
 		
-	// 	for (var i = 0; i < l.length; i++) {
-	// 		var childTable = cur_frm.add_child("import_cost_sheet_details");
-	// 		console.log(i)
-	// 		childTable.parameters = l[i]
-	// 	}
-	// 	cur_frm.refresh_fields("import_cost_sheet_details");
-	// },
+		for (var i = 0; i < l.length; i++) {
+			var childTable = cur_frm.add_child("import_cost_sheet_items");
+			console.log(i)
+			childTable.items = l[i]
+		}
+		cur_frm.refresh_fields("import_cost_sheet_items");
+		}
+	},
 	purchase_invoice: function(frm){
 	
 		frappe.call({
@@ -31,47 +33,48 @@ frappe.ui.form.on('Import Cost Sheet', {
 		for (var i=0; i<success.message.length; i++){
 			let row = frm.add_child('import_cost_sheet_details')
 			row.parameters= success.message[i].item_code
+			row.amount = success.message[i].amount
 			
-				console.log(success.message[i])
-				switch(success.message[i].item_name) {
-					case "Sea Fright":
-						row.sea_fright_etb = success.message[i].amount
-						break;
-					case "Inland Fright":
-						row.inland_fright_etb = success.message[i].amount
-					    break;
-					case "Insurance":
-						row.insurance_etb = success.message[i].amount
-					  	break;
-					case "Import Customs Duty":
-						row.import_customs_duty_etb = success.message[i].amount
-						break;
-					case "Other":
-						row.other_etb = success.message[i].amount
-					  	break;
-					case "Bank charge":
-						row.bank_charge_etb = success.message[i].amount
-					  	break;
-					case "Storage":
-						row.storage_etb = success.message[i].amount
-						break;
-					case "Port handling charge":
-						row.port_handling_charge_etb = success.message[i].amount
-					  	break;
-					case "Transit and clearing":
-						row.transit_and_clearing_etb = success.message[i].amount
-					  	break;
-					case "Loading and unloading":
-						row.loading_and_unloading_etb = success.message[i].amount
-						break;
-					case "Inland transport":
-						row.inland_transport_etb = success.message[i].amount
-					  	break;
-					case "Miscellaneous":
-						row.miscellaneous_etb = success.message[i].amount
-					  	break;
-				  }
-			row.amount = total_amount
+			// 	console.log(success.message[i])
+			// 	switch(success.message[i].item_name) {
+			// 		case "Sea Fright":
+			// 			row.sea_fright_etb = success.message[i].amount
+			// 			break;
+			// 		case "Inland Fright":
+			// 			row.inland_fright_etb = success.message[i].amount
+			// 		    break;
+			// 		case "Insurance":
+			// 			row.insurance_etb = success.message[i].amount
+			// 		  	break;
+			// 		case "Import Customs Duty":
+			// 			row.import_customs_duty_etb = success.message[i].amount
+			// 			break;
+			// 		case "Other":
+			// 			row.other_etb = success.message[i].amount
+			// 		  	break;
+			// 		case "Bank charge":
+			// 			row.bank_charge_etb = success.message[i].amount
+			// 		  	break;
+			// 		case "Storage":
+			// 			row.storage_etb = success.message[i].amount
+			// 			break;
+			// 		case "Port handling charge":
+			// 			row.port_handling_charge_etb = success.message[i].amount
+			// 		  	break;
+			// 		case "Transit and clearing":
+			// 			row.transit_and_clearing_etb = success.message[i].amount
+			// 		  	break;
+			// 		case "Loading and unloading":
+			// 			row.loading_and_unloading_etb = success.message[i].amount
+			// 			break;
+			// 		case "Inland transport":
+			// 			row.inland_transport_etb = success.message[i].amount
+			// 		  	break;
+			// 		case "Miscellaneous":
+			// 			row.miscellaneous_etb = success.message[i].amount
+			// 		  	break;
+			// 	  }
+			// row.amount = total_amount
 	    }
 		  frm.refresh_field('import_cost_sheet_details');
 		})
--- ethal/ethal/doctype/import_cost_sheet/import_cost_sheet.py
+++ ethal/ethal/doctype/import_cost_sheet/import_cost_sheet.py
@@ -7,7 +7,16 @@ import frappe
 from frappe.model.document import Document
 
 class ImportCostSheet(Document):
-	pass
+	def after_insert(self):
+		print("ja na be", self.name)
+		items = frappe.db.get_all('Import Cost Sheet Items', {'parent': self.name}, ['items', 'amount'])
+		for i in items:
+			if i['items'] == 'Sea Fright (ETB)':
+				print(i['amount'])
+				a = frappe.db.get_all('Import Cost Sheet Details', {'parent': self.name}, ['idx'], as_list=True)
+				print(a)
+		frappe.throw('ja na be')
+
 
 @frappe.whitelist()
 def get_value(name):
