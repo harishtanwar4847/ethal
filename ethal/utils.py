533d9edcc98fd01a897f603496d3274ea7d94790
halimasayyed <halima.sayyed@atriina.com>
2021-02-16 13:19:11 +0530

    remove comment

--- ethal/utils.py
+++ ethal/utils.py
@@ -83,67 +83,6 @@ def set_approver_name(doc, method):
 def before_submit_stock_entry(doc, method):
     if doc.value_difference > 1:
         frappe.throw('Incoming Value not equal to Outgoing Value! Please Correct the rate.')
-        
-# @frappe.whitelist()
-# def assign_salary_structure(doc, company=None, grade=None, department=None, designation=None,employee=None,
-#         from_date=None, base=None, variable=None, income_tax_slab=None):
-#     employees = get_employees(doc, company= company, grade= grade,department= department,designation= designation,name=employee)
-
-#     if employees:
-#         if len(employees) > 20:
-#             frappe.enqueue(assign_salary_structure_for_employees, timeout=600,
-#                 employees=employees, salary_structure=doc,from_date=from_date,
-#                 base=base, variable=variable, income_tax_slab=income_tax_slab)
-#         else:
-#             assign_salary_structure_for_employees(employees, doc, from_date=from_date,
-#                 base=base, variable=variable, income_tax_slab=income_tax_slab)
-#     else:
-#         frappe.msgprint(frappe._("No Employee Found"))
-
-# def assign_salary_structure_for_employees(employees, salary_structure, from_date=None, base=None, variable=None, income_tax_slab=None):
-# 	salary_structures_assignments = []
-# 	existing_assignments_for = get_existing_assignments(employees, salary_structure, from_date)
-# 	count=0
-# 	for employee in employees:
-# 		if employee in existing_assignments_for:
-# 			continue
-# 		count +=1
-
-# 		salary_structures_assignment = create_salary_structures_assignment(employee,
-# 			salary_structure, from_date, base, variable, income_tax_slab)
-# 		salary_structures_assignments.append(salary_structures_assignment)
-# 		frappe.publish_progress(count*100/len(set(employees) - set(existing_assignments_for)), title = frappe._("Assigning Structures..."))
-
-# 	if salary_structures_assignments:
-# 		frappe.msgprint(frappe._("Structures have been assigned successfully"))
-
-
-# def create_salary_structures_assignment(employee, salary_structure, from_date, base, variable, income_tax_slab=None):
-#     salary_structure = ast.literal_eval(salary_structure)
-#     assignment = frappe.new_doc("Salary Structure Assignment")
-#     assignment.employee = employee
-#     assignment.salary_structure = salary_structure['name']
-#     assignment.company = salary_structure['company']
-#     assignment.from_date = from_date
-#     assignment.base = base
-#     assignment.variable = variable
-#     assignment.income_tax_slab = income_tax_slab
-#     assignment.save(ignore_permissions = True)
-#     assignment.submit()
-#     return assignment.name
-
-
-# def get_existing_assignments(employees, salary_structure, from_date):
-#     salary_structure = ast.literal_eval(salary_structure)
-#     salary_structures_assignments = frappe.db.sql_list("""
-#         select distinct employee from `tabSalary Structure Assignment`
-#         where salary_structure=%s and employee in (%s)
-#         and company= %s and docstatus=1
-#     """ % ('%s', ', '.join(['%s']*len(employees)),'%s'), [salary_structure['name']] + employees+[salary_structure['company']])
-#     if salary_structures_assignments:
-#         frappe.msgprint(frappe._("Skipping Salary Structure Assignment for the following employees, as Salary Structure Assignment records already exists against them. {0}")
-#             .format("\n".join(salary_structures_assignments)))
-#     return salary_structures_assignments
 
 @frappe.whitelist()
 def make_salary_slip(source_name, target_doc = None, employee = None, as_print = False, print_format = None, for_preview=0, ignore_permissions=False):
@@ -173,22 +112,6 @@ def make_salary_slip(source_name, target_doc = None, employee = None, as_print =
 		return frappe.get_print(doc.doctype, doc.name, doc = doc, print_format = print_format)
 	else:
 		return doc
-
-
-# def get_employees(doc, **kwargs):
-# 		conditions, values = [], []
-# 		for field, value in kwargs.items():
-# 			if value:
-# 				conditions.append("{0}=%s".format(field))
-# 				values.append(value)
-
-# 		condition_str = " and " + " and ".join(conditions) if conditions else ""
-
-# 		employees = frappe.db.sql_list("select name from tabEmployee where status='Active' {condition}"
-# 			.format(condition=condition_str), tuple(values))
-
-# 		return employees
-
 
 @frappe.whitelist()
 def before_insert_payment_entry(doc, method):
