dd2cc7c2ff681770e9f3400f2c16c52f9b544b55
halimasayyed <halima.sayyed@atriina.com>
2021-03-01 23:14:59 +0530

    show data without filters too

--- ethal/ethal/report/melting_furnace/melting_furnace.py
+++ ethal/ethal/report/melting_furnace/melting_furnace.py
@@ -6,16 +6,24 @@ import frappe
 
 def execute(filters=None):
 	columns, data = [], []
-	columns = ["MF:Data:100"]+["Rolling Ingot Weight:Float:100"]+["No of Ingots per heat:int:100"]+["Shift A - In Nos:Int:100"]+["Shift A - In Kg:Float:100"]+["Shift B - In Nos:Int:100"]+["Shift B - In Kg:Float:100"]+["Shift C - In Nos:Int:100"]+["Shift C - In Kg:Float:100"]+["Total Pieces:Int:100"]+["Total Weight:Float:100"]+["Heats:Int:100"]
+	columns = ["Melting Furnace:Link/Melting Furnace:200"]+["MF:Data:100"]+["Rolling Ingot Weight:Float:100"]+["No of Ingots per heat:int:100"]+["Shift A - In Nos:Int:100"]+["Shift A - In Kg:Float:100"]+["Shift B - In Nos:Int:100"]+["Shift B - In Kg:Float:100"]+["Shift C - In Nos:Int:100"]+["Shift C - In Kg:Float:100"]+["Total Pieces:Int:100"]+["Total Weight:Float:100"]+["Heats:Int:100"]
 	data = get_date(filters)
 	return columns, data
 
 def get_date(filters):
-	return frappe.db.sql("""
-		select mf, cast(rolling_ingot_weight as decimal(10,6)), cast(no_of_ingots_per_heat as int), shift_a_in_nos, shift_a_in_kg, 
-			shift_b_in_nos, shift_b_in_kg, shift_c_in_nos, shift_c_in_kg, total_pieces, total_weight, heats
-		from `tabMelting Furnace Items`	
-		where parent = '{0}'	
-		order by idx	
-	""".format(filters.melting_furnace))
+	if filters.melting_furnace:
+		return frappe.db.sql("""
+			select parent, mf, cast(rolling_ingot_weight as decimal(10,6)), cast(no_of_ingots_per_heat as int), shift_a_in_nos, shift_a_in_kg, 
+				shift_b_in_nos, shift_b_in_kg, shift_c_in_nos, shift_c_in_kg, total_pieces, total_weight, heats
+			from `tabMelting Furnace Items`	
+			where parent = '{0}'	
+			order by idx	
+		""".format(filters.melting_furnace))
+	else:
+		return frappe.db.sql("""
+			select parent, mf, cast(rolling_ingot_weight as decimal(10,6)), cast(no_of_ingots_per_heat as int), shift_a_in_nos, shift_a_in_kg, 
+				shift_b_in_nos, shift_b_in_kg, shift_c_in_nos, shift_c_in_kg, total_pieces, total_weight, heats
+			from `tabMelting Furnace Items`	
+			order by idx	
+		""")	
 
