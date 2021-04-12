4d3a8b2a808bd56a9c91f4c5374f18eb5389f8e5
halimasayyed <halima.sayyed@atriina.com>
2021-04-12 12:40:45 +0530

    add item name column

--- ethal/ethal/report/qc/qc.py
+++ ethal/ethal/report/qc/qc.py
@@ -6,14 +6,14 @@ import frappe
 
 def execute(filters=None):
 	columns, data = [], []
-	columns = ["QC:Link/QC:200"]+["Item Name:Link/Item:250"]+["Rejection Ash:Float:100"]+["Rejection Blister:Float:100"]+["Rejection Rolling:Float:100"]+["Rejection Others:Float:100"]+["OK Circle Received:Float:100"]+["OK Circle Received(Pieces):Int:100"]+["Total Rejection:Float:100"]+["Melting Rejection:Float:100"]+["Total Circle Received:Float:100"]
+	columns = ["QC:Link/QC:200"]+["Item:Link/Item:250"]+["Item Name:Data:300"]+["Rejection Ash:Float:100"]+["Rejection Blister:Float:100"]+["Rejection Rolling:Float:100"]+["Rejection Others:Float:100"]+["OK Circle Received:Float:100"]+["OK Circle Received(Pieces):Int:100"]+["Total Rejection:Float:100"]+["Melting Rejection:Float:100"]+["Total Circle Received:Float:100"]
 	data = get_data(filters)
 	return columns, data
 
 def get_data(filters):
 	if filters.qc:
 		return frappe.db.sql("""
-			select parent, item_name, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
+			select parent, item_name, item, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
 				ok_circle_receivedpieces, total_rejection, melting_rejection, total_circle_received
 			from `tabQC Items` 
 			where parent = '{0}'
@@ -21,7 +21,7 @@ def get_data(filters):
 		""".format(filters.qc))
 	elif filters.from_date and filters.to_date:
 		return frappe.db.sql("""
-			select parent, item_name, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
+			select parent, item_name, item, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
 				ok_circle_receivedpieces, total_rejection, melting_rejection, total_circle_received
 			from `tabQC Items` 
 			where creation between '{0}' and '{1}'
@@ -29,7 +29,7 @@ def get_data(filters):
 		""".format(filters.from_date, filters.to_date))
 	elif filters.qc and filters.from_date and filters.to_date:
 		return frappe.db.sql("""
-			select parent, item_name, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
+			select parent, item_name, item, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
 				ok_circle_receivedpieces, total_rejection, melting_rejection, total_circle_received
 			from `tabQC Items` 
 			where parent = '{0}'
@@ -38,7 +38,7 @@ def get_data(filters):
 		""".format(filters.qc, filters.from_date, filters.to_date))
 	else: 
 		return frappe.db.sql("""
-			select parent, item_name, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
+			select parent, item_name, item, rejection_ash, rejection_blister, rejection_rolling, rejection_others, ok_circle_received,
 				ok_circle_receivedpieces, total_rejection, melting_rejection, total_circle_received
 			from `tabQC Items` 
 			order by parent	
