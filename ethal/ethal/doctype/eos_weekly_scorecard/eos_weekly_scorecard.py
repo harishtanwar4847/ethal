49684eb9c9e2b44f80d70073ead13822c409aa02
halimasayyed <halima.sayyed@atriina.com>
2021-06-07 17:24:37 +0530

    set despense-utils actual value on the basis of sales invoice total net weight

--- ethal/ethal/doctype/eos_weekly_scorecard/eos_weekly_scorecard.py
+++ ethal/ethal/doctype/eos_weekly_scorecard/eos_weekly_scorecard.py
@@ -13,6 +13,29 @@ class EOSWeeklyScorecard(Document):
 				a= val.idx - 2
 				previous_values = self.eos_details[a].actual / self.eos_details[a].target if self.eos_details[a].actual and self.eos_details[a].target else 0
 				val.actual = previous_values * 100
+			if val.division == 'UD-TU' and val.parameter == 'Despatch - Utensils':
+				sales_invoice = frappe.db.get_all('Sales Invoice', filters={'posting_date': [ 'between', [self.from_date, self.to_date]], 'naming_series': ['like', '%'+'TU'+'%'] }, fields=['name'])	
+				net_weight_total = 0
+				if sales_invoice:
+					for i in sales_invoice:
+						net_weight_amount = frappe.db.sql("""
+							select sum(si.total_net_weight) 
+							from `tabSales Invoice Item` as si
+							join `tabItem` as i
+							on si.item_code =  i.name
+							where i.item_group = 'FINISHED GOODS' and si.parent = '{0}'
+						""".format(i['name']))
+						net_weight_total += net_weight_amount[0][0]
+				val.actual = net_weight_total	
+
+
+
+
+
+
+
+
+
 
 @frappe.whitelist()
 def get_previous_record(doc):
