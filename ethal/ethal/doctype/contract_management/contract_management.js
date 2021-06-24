4e18c586231e1343ae53571c39fff7a49dbdf4c2
halimasayyed <halima.sayyed@atriina.com>
2021-06-24 12:21:38 +0530

    fix cotract for issue

--- ethal/ethal/doctype/contract_management/contract_management.js
+++ ethal/ethal/doctype/contract_management/contract_management.js
@@ -12,14 +12,22 @@ frappe.ui.form.on('Contract Management', {
 		})
 	},
 	contract_type: function(frm) {
-		if (frm.doc.contract_type == 'Insurance') {
-			frm.set_df_property('contract_for', 'options', ['Motor Vehicle - Full', 'Motor Vehicle - 3rd Party', 'Fixed Asset', 'Current Asset', 'Goods in Transit', 'Fidelity', 'Workmens', 'Others'])
-		}
-		else if (frm.doc.contract_type == 'Licenses') {
-			frm.set_df_property('contract_for', 'options', ['Business Licence', 'Investment Permit', 'TIN no', 'VAT', 'ESIA'])
-		}
-		else if (frm.doc.contract_type == 'Employee Permits') {
-			frm.set_df_property('contract_for', 'options', ['Passport', 'WorkPermit', 'Temporary Residence ID', 'Visa', 'Yellow Fever Vaccination Certificate', 'Oral Polio vaccination Certificate'])
-		}
+		load_contract_type(frm)
+	},
+	refresh: function(frm) {
+		console.log('hello')
+		load_contract_type(frm)
 	}
 });
+
+function load_contract_type(frm) {
+	if (frm.doc.contract_type == 'Insurance') {
+		frm.set_df_property('contract_for', 'options', ['Motor Vehicle - Full', 'Motor Vehicle - 3rd Party', 'Fixed Asset', 'Current Asset', 'Goods in Transit', 'Fidelity', 'Workmens', 'Others'])
+	}
+	else if (frm.doc.contract_type == 'Licenses') {
+		frm.set_df_property('contract_for', 'options', ['Business Licence', 'Investment Permit', 'TIN no', 'VAT', 'ESIA'])
+	}
+	else if (frm.doc.contract_type == 'Employee Permits') {
+		frm.set_df_property('contract_for', 'options', ['Passport', 'WorkPermit', 'Temporary Residence ID', 'Visa', 'Yellow Fever Vaccination Certificate', 'Oral Polio vaccination Certificate'])
+	}
+}
