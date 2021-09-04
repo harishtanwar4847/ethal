7498ba34929be64d9f8b95da5a673d39b48b299c
halimasayyed <halima.sayyed@atriina.com>
2021-09-04 14:41:42 +0530

    use try except in hit telegram send message api method

--- ethal/ethal/doctype/telegram_bot_settings/telegram_bot_settings.py
+++ ethal/ethal/doctype/telegram_bot_settings/telegram_bot_settings.py
@@ -7,23 +7,21 @@ from frappe.model.document import Document
 
 class TelegramBotSettings(Document):
 	def send_telegram_message(self, message, group):
-		try:
-			if not self.bot_api_token:
-				return
-			else:
-				group_id = None
-				if group == 'Sales':
-					group_id = self.sales_group_id
-				elif group == 'Purchase':
-					group_id = self.purchase_group_id
-				elif group == 'Stock':
-					group_id = self.stock_group_id
+		group_id = None
+		if not self.bot_api_token:
+			return
+		if group == 'Sales':
+			group_id = self.sales_group_id
+		elif group == 'Purchase':
+			group_id = self.purchase_group_id
+		elif group == 'Stock':
+			group_id = self.stock_group_id
 
-				self.hit_telegram_send_message_api(group_id, message)	
-
-		except requests.exceptions.RequestException:
-			frappe.log_error(title='Telegram Bot API Hit error')
-					
+		self.hit_telegram_send_message_api(group_id, message)			
 
 	def hit_telegram_send_message_api(self, group_id, message):
-		response = requests.get('https://api.telegram.org/{}/sendMessage?chat_id={}&text={}'.format(self.bot_api_token, group_id, message))			
+		try:
+			encode_message = urllib.parse.quote(message)
+			response = requests.get('https://api.telegram.org/{}/sendMessage?chat_id={}&text={}'.format(self.bot_api_token, group_id, encode_message))			
+		except requests.exceptions.RequestException:
+			frappe.log_error(title='Telegram Bot API Hit error')	
