3178fd75b456b6923267c91e73afd8557553ee66
halimasayyed <halima.sayyed@atriina.com>
2021-09-30 21:55:11 +0530

    refactor

--- ethal/ethal/report/attendance_report/attendance_report.py
+++ ethal/ethal/report/attendance_report/attendance_report.py
@@ -10,83 +10,27 @@ def execute(filters=None):
 	return columns, data
 
 def get_data(filters):
-	if 'department' in filters and 'working_area' in filters and 'shift' not in filters and 'status' not in filters:
-		print('in department and working area')
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and att.department = '{}' and emp.working_area = '{}'".format(filters['from_date'], filters['to_date'], filters['department'], filters['working_area'])
-		return query_condition(condition)	
-	elif 'department' not in filters and 'working_area' in filters and 'shift' not in filters and 'status' not in filters:
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and emp.working_area = '{}'".format(filters['from_date'], filters['to_date'], filters['working_area'])
-		print('in working area')
-		return query_condition(condition)
-	elif 'department' not in filters and 'working_area' not in filters and 'shift' in filters and 'status' not in filters:
-		print('in shift')
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and att.shift = '{}'".format(filters['from_date'], filters['to_date'], filters['shift'])
-		return query_condition(condition)
-	elif 'department' in filters and 'working_area' not in filters and 'shift' not in filters and 'status' not in filters:
-		print('in department')
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and att.department = '{}'".format(filters['from_date'], filters['to_date'], filters['department'])
-		return query_condition(condition)	
-	elif 'department' not in filters and 'working_area' not in filters and 'shift' not in filters and 'status' in filters:
-		print('in status')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}'".format(filters['status'], filters['from_date'], filters['to_date'])
-		return query_condition(condition)							
-	elif 'department' in filters and 'shift' in filters and 'working_area' in filters and 'status' not in filters:
-		print('department and shift and working area')
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and att.department = '{}' and att.shift = '{}' and emp.working_area = '{}'".format(filters['from_date'], filters['to_date'], filters['department'], filters['shift'], filters['working_area'])
-		return query_condition(condition)
-	elif 'department' in filters and 'shift' in filters and 'working_area' not in filters and 'status' in filters:
-		print('department and shift and status')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}' and att.department = '{}' and att.shift = '{}'".format(filters['status'], filters['from_date'], filters['to_date'], filters['department'], filters['shift'])
-		return query_condition(condition)
-	elif 'department' in filters and 'shift' not in filters and 'working_area' in filters and 'status' in filters:
-		print('department and status and working area')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}' and att.department = '{}' and emp.working_area = '{}'".format(filters['status'], filters['from_date'], filters['to_date'], filters['department'], filters['working_area'])
-		return query_condition(condition)
-	elif 'department' not in filters and 'shift' in filters and 'working_area' in filters and 'status' in filters:
-		print('shift and status and working area')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}' and emp.working_area = '{}' and att.shift = '{}'".format(filters['status'], filters['from_date'], filters['to_date'], filters['working_area'], filters['shift'])
-		return query_condition(condition)
-	elif 'department' in filters and 'shift' in filters and 'working_area' in filters and 'status' in filters:
-		print('shift and status and working area and department')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}' and att.department = '{}' and emp.working_area = '{}' and att.shift = '{}'".format(filters['status'], filters['from_date'], filters['to_date'], filters['department'], filters['working_area'], filters['shift'])
-		return query_condition(condition)
-	elif 'department' in filters and 'shift' not in filters and 'working_area' in filters and 'status' not in filters:
-		print('department and working area')
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and att.department = '{}' and emp.working_area = '{}'".format(filters['from_date'], filters['to_date'], filters['department'], filters['working_area'])
-		return query_condition(condition)
-	elif 'department' not in filters and 'shift' in filters and 'working_area' in filters and 'status' not in filters:
-		print('shift and working area')
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and att.shift = '{}' and emp.working_area = '{}'".format(filters['from_date'], filters['to_date'], filters['shift'], filters['working_area'])
-		return query_condition(condition)
-	elif 'department' in filters and 'shift' in filters and 'working_area' not in filters and 'status' not in filters:
-		print('shift and department area')
-		condition = "where att.docstatus = 1 and att.attendance_date between '{}' and '{}' and att.shift = '{}' and att.department = '{}'".format(filters['from_date'], filters['to_date'], filters['shift'], filters['department'])
-		return query_condition(condition)
-	elif 'department' not in filters and 'shift' in filters and 'working_area' not in filters and 'status' in filters:
-		print('shift and status ')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}' and att.shift = '{}'".format(filters['status'], filters['from_date'], filters['to_date'], filters['shift'])
-		return query_condition(condition)	
-	elif 'department' in filters and 'shift' not in filters and 'working_area' not in filters and 'status' in filters:
-		print('department and status ')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}' and att.department = '{}'".format(filters['status'], filters['from_date'], filters['to_date'], filters['department'])
-		return query_condition(condition)
-	elif 'department' not in filters and 'shift' not in filters and 'working_area' in filters and 'status' in filters:
-		print('working area and status ')
-		condition = "where att.docstatus = 1 and att.status = '{}' and att.attendance_date between '{}' and '{}' and emp.working_area = '{}'".format(filters['status'], filters['from_date'], filters['to_date'], filters['working_area'])
-		return query_condition(condition)	
-	else:			
-		print('in else')	
-		condition = "where att.docstatus = 1 and att.status = '{}' and attendance_date between '{}' and '{}'".format(filters['status'], filters['from_date'], filters['to_date'])
-		return query_condition(condition)
+	where_clauses = [" where att.attendance_date BETWEEN '{}' AND {}".format(filters['from_date'], filters['to_date'])]
 
-def query_condition(condition):
+	if 'status' in filters:
+		where_clauses.append("att.status = '{}'".format(filters['status']))
+
+	if 'shift' in filters:
+		where_clauses.append("att.shift = '{}'".format(filters['shift']))
+
+	if 'working_area' in filters:
+		where_clauses.append("emp.working_area = '{}'".format(filters['working_area']))
+
+	if 'status' in filters:
+		where_clauses.append("att.status = '{}'".format(filters['status']))
+
 	query = frappe.db.sql("""
 		select att.employee, att.employee_name, att.attendance_date, att.status, att.department, att.shift, att.working_hours 
 		from `tabAttendance` att
 		join `tabEmployee` emp
 		on att.employee = emp.name
 		{}
-		""".format(condition), as_dict=1)
+		""".format(' AND '.join(where_clauses)), as_dict=1)
 	if query:
 		for i in query:
 			shift = 0
@@ -106,4 +50,4 @@ def query_condition(condition):
 					and eibd.employee = '{1}' and eib.salary_component = 'Production Incentive' and eib.docstatus = 1
 			""".format(i['attendance_date'], i['employee']))
 			i['incentive'] = employee_incentive[0][0]		
-		return query
+		return frappe.db.sql(query)
