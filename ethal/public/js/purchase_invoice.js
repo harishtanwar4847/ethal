d741acff02d2a9134335d3c33da66067457a3626
ankit-atriina <ankit.patil@atriina.com>
2022-09-15 09:27:37 +0000

    Fix:E-tax js changes and custom field added

--- ethal/public/js/purchase_invoice.js
+++ ethal/public/js/purchase_invoice.js
@@ -1,11 +1,11 @@
 frappe.ui.form.on("Purchase Invoice Item", "item_code", function(frm, cdt, cdn) {
     var d = locals[cdt][cdn];
         frappe.db.get_value("Item", {"name": d.item_code}, "item_group", function(value) {
-            if (value.item_group == "Services"){
-                d.type_of_purchase = "SERVICES";
+            if (value.item_group == "SERVICES"){
+                d.type_of_purchase = "Services";
             }
             else(
-                d.type_of_purchase = "PURCHASE"
+                d.type_of_purchase = "Purchase"
             )
             
         });
